34345142ce91dbc48a3068ebf598c07d19d2628f
Travis Cummings <travis@istalusconsulting.com>
2016-01-28 11:18:50 -0700

    Refactored client scripts into their own projects

---
+++ src/main/resources/db/openidconnect/mysql/pediatric-growth-chart-client.sql
@@ -0,0 +1,20 @@
+USE oic;
+
+SET AUTOCOMMIT = 0;
+
+START TRANSACTION;
+
+-- Pediatric Growth Chart
+INSERT INTO client_details (client_id, client_name, logo_uri, access_token_validity_seconds, token_endpoint_auth_method) VALUES
+	('growth_chart', 'Pediatric Growth Chart', 'http://localhost:8080/hsp-reference-apps/static/images/apps/growth-chart.png', 86400, 'NONE');
+
+INSERT INTO client_scope (owner_id, scope) VALUES
+	((SELECT id from client_details where client_id = 'growth_chart'), 'launch'),
+	((SELECT id from client_details where client_id = 'growth_chart'), 'patient/*.read');
+
+INSERT INTO client_grant_type (owner_id, grant_type) VALUES
+	((SELECT id from client_details where client_id = 'growth_chart'), 'authorization_code');
+
+COMMIT;
+
+SET AUTOCOMMIT = 1;
