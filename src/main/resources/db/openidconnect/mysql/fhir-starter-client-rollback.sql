31c58b757ad6ebc746102630d7957c8b2ed244d0
Travis Cummings <travis@istalusconsulting.com>
2016-01-28 22:12:49 -0700

    Removed creation of OIC.  Need to create that outside of script

--- src/main/resources/db/openidconnect/mysql/fhir-starter-client-rollback.sql
+++ src/main/resources/db/openidconnect/mysql/fhir-starter-client-rollback.sql
@@ -3,12 +3,6 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
 -- SMART on FHIR Application Launcher
-INSERT INTO client_details (client_id, client_name, access_token_validity_seconds) VALUES
-	('fhir_starter', 'FHIR Starter', 86400);
-
-INSERT INTO client_redirect_uri (owner_id, redirect_uri) VALUES
-	((SELECT id from client_details where client_id = 'fhir_starter'), 'http://localhost:8080/hspc-reference-apps');
-
 DELETE FROM whitelisted_site_scope WHERE owner_id = (SELECT id from client_details where client_id = 'fhir_starter');
 DELETE FROM whitelisted_site WHERE client_id = 'fhir_starter';
 DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'fhir_starter');
