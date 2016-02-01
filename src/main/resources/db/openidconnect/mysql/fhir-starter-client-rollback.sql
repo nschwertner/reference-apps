a47f841c06b4de4ff1195752b5ff2b0d4d975cb4
amy6 <amy@motibox.com>
2016-02-01 08:59:33 -0500

    Fixed rollback mysql script

--- src/main/resources/db/openidconnect/mysql/fhir-starter-client-rollback.sql
+++ src/main/resources/db/openidconnect/mysql/fhir-starter-client-rollback.sql
@@ -3,7 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
 -- SMART on FHIR Application Launcher
-DELETE FROM whitelisted_site_scope WHERE owner_id = (SELECT id from client_details where client_id = 'fhir_starter');
+DELETE FROM whitelisted_site_scope WHERE owner_id = (SELECT id from whitelisted_site where client_id = 'fhir_starter');
 DELETE FROM whitelisted_site WHERE client_id = 'fhir_starter';
 DELETE FROM client_grant_type WHERE owner_id = (SELECT id from client_details where client_id = 'fhir_starter');
 DELETE FROM client_scope WHERE owner_id = (SELECT id from client_details where client_id = 'fhir_starter');
