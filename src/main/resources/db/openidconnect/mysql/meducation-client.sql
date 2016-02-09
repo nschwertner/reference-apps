e85d0b692bf03c84ba8f6d3e6aa20c13c570ee76
Travis Cummings <travis@istalusconsulting.com>
2016-02-08 17:51:44 -0700

    Added image logo for Meducation

--- src/main/resources/db/openidconnect/mysql/meducation-client.sql
+++ src/main/resources/db/openidconnect/mysql/meducation-client.sql
@@ -3,7 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
 INSERT INTO client_details (client_id, client_name, access_token_validity_seconds, logo_uri, token_endpoint_auth_method) VALUES
-	('meducation', 'Meducation RS', 86400, '', 'NONE');
+	('meducation', 'Meducation RS', 86400, 'https://fhir-dstu2.meducation.com/images/meducation_icon.png', 'NONE');
 
 INSERT INTO client_redirect_uri (owner_id, redirect_uri) VALUES
 	((SELECT id from client_details where client_id = 'meducation'), 'https://fhir-dstu2.meducation.com/index.html');
