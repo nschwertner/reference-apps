255a0a26934ed1d93ab33424b209cf4f62d02b26
Travis Cummings <travis@istalusconsulting.com>
2016-02-25 12:32:08 -0700

    Added logo for Crimson Care Management, enhanced CDS hook

--- src/main/resources/db/openidconnect/mysql/crimson-care-management-client.sql
+++ src/main/resources/db/openidconnect/mysql/crimson-care-management-client.sql
@@ -3,7 +3,7 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 
 INSERT INTO client_details (client_id, client_name, access_token_validity_seconds, logo_uri, token_endpoint_auth_method) VALUES
-	('d827bd57-1be6-49a6-8f23-577c7edb51df', 'Crimson Care Management', 86400, '', 'NONE');
+	('d827bd57-1be6-49a6-8f23-577c7edb51df', 'Crimson Care Management', 86400, 'https://sandbox.hspconsortium.org/dstu2/hspc-reference-apps/static/images/apps/ccm_homepage_icon.png', 'NONE');
 
 INSERT INTO client_redirect_uri (owner_id, redirect_uri) VALUES
 	((SELECT id from client_details where client_id = 'd827bd57-1be6-49a6-8f23-577c7edb51df'), 'https://fhirdemo.advisory.com/launcherHspc');
